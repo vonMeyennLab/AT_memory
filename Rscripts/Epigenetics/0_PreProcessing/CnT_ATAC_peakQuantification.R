9072243c60cf9cb5b2998647af023268876e7d29
Laura C. Hinte <laurahinte@gmail.com>
2024-10-02 10:21:53 +0200

    Update CnT_ATAC_peakQuantification.R
    updated figure references

--- Rscripts/Epigenetics/0_PreProcessing/CnT_ATAC_peakQuantification.R
+++ Rscripts/Epigenetics/0_PreProcessing/CnT_ATAC_peakQuantification.R
@@ -1,6 +1,6 @@
 
 # This R script has been used to generate results used in 
-# Figures 4, 5 and associated extended data
+# Figures 3-6 and associated extended data
 
 library(chromVAR)
 library(GenomicRanges)
